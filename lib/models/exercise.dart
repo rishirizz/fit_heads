ae8aa9bb5ac31f97ac16253f78f6d3b234e044c5
arijeet <arijeetc6@gmail.com>
2023-11-17 11:20:35 +0530

    Created workout and exercise models.

---
+++ lib/models/exercise.dart
@@ -0,0 +1,15 @@
+class Exercise {
+  final String name;
+  final String weight;
+  final String reps;
+  final String sets;
+  final bool isCompleted;
+
+  Exercise({
+    required this.name,
+    required this.weight,
+    required this.reps,
+    required this.sets,
+    required this.isCompleted,
+  });
+}
